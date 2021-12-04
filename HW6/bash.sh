43973605f6ca304d38aee751b2ea096f04c0bbcf
eesalt <eesalt@gmail.com>
2021-12-03 21:27:48 -0800

    Commenting my code to make it clearer

--- HW6/bash.sh
+++ HW6/bash.sh
@@ -4,6 +4,7 @@ docker-compose up db-service -d
 docker cp baseball.sql db-service:/baseball.sql
 docker cp HW2.sql db-service:/HW2.sql
 
+#Thanks to Will for help here as well
 if ! docker container exec -i db-service mysql -uroot -psecret -e'use baseball';
 then
   docker container exec -i db-service mysql -uroot -psecret -e'create database baseball';
